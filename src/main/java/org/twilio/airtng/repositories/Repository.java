b7f3c38a9aba284b9e85453361f54e372efdf5c0
Carolina Lopez <calopez@twilio.com>
2021-10-21 18:00:15 -0500

    Implement dotenv-java

--- src/main/java/org/twilio/airtng/repositories/Repository.java
+++ src/main/java/org/twilio/airtng/repositories/Repository.java
@@ -1,5 +1,8 @@
 package org.twilio.airtng.repositories;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.twilio.airtng.lib.Config;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
@@ -96,20 +99,19 @@ public abstract class Repository<T> {
     }
 
     private Map<String, String> getProperties() {
-        Map<String, String> env = System.getenv();
+        Dotenv dotenv = Config.getDotenv();
         Map<String, String> config = new HashMap<>();
-        for (String key : env.keySet()) {
-            if (key.contains("JDBC_URL")) {
-                config.put("javax.persistence.jdbc.url", env.get(key));
-            }
 
-            if (key.contains("DB_USER")) {
-                config.put("javax.persistence.jdbc.user", env.get(key));
-            }
+        if (dotenv.get("JDBC_URL") != null) {
+            config.put("javax.persistence.jdbc.url", dotenv.get("JDBC_URL"));
+        }
 
-            if (key.contains("DB_PASSWORD")) {
-                config.put("javax.persistence.jdbc.password", env.get(key));
-            }
+        if (dotenv.get("DB_USER") != null) {
+            config.put("javax.persistence.jdbc.user", dotenv.get("DB_USER"));
+        }
+
+        if (dotenv.get("DB_PASSWORD") != null) {
+            config.put("javax.persistence.jdbc.password", dotenv.get("DB_PASSWORD"));
         }
 
         return config;
